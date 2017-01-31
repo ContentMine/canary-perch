b33941d6c4400501bbfdb3d005b14d1d858388ae
Thomas Arrow <thomasarrow@gmail.com>
2017-01-31 14:46:53 +0000

    convert extract.js to use extractor object

--- extract.js
+++ extract.js
@@ -8,23 +8,31 @@ var _ = require('lodash')
 var numberOfFiles
 var finished
 
-var readDictionaries = function (dailyset, hosts, dictsdir, storedir) {
-  var folder = dictsdir + '/json/'
-  var client = index.ESClient(hosts)
+var extractor = function (hosts, inputIndex, outputIndex, inputType, outputType, dictsDir) {
+  this.hosts = hosts
+  this.inputIndex = inputIndex
+  this.outputIndex = outputIndex
+  this.inputType = inputType
+  this.outputType = outputType
+  this.dictsDir = dictsDir
+}
+
+extractor.prototype.readDictionaries = function () {
+  var Extractor = this
+  var folder = Extractor.dictsDir + '/json/'
+  var client = index.ESClient(Extractor.hosts)
   console.log('starting extraction')
   recursive(folder, function (err, files) {
     if (err) throw err
     numberOfFiles = files.length
     finished = _.after(numberOfFiles, () => {
-      fs.unlink(storedir + '/elasticsearch.lock', (err) => {
-        if (err) throw err
-        console.log('all extractions finished')
-      })
+      if (err) throw err
+      console.log('all extractions finished')
     })
     files.forEach(function (file) {
       fs.readFile(file, 'utf8', function (err, data) {
         if (err) throw err
-        dictionaryQuery(JSON.parse(data), dailyset, client)
+        Extractor.dictionaryQuery(JSON.parse(data), client)
       })
     })
   })
@@ -32,11 +40,12 @@ var readDictionaries = function (dailyset, hosts, dictsdir, storedir) {
 
 // Pass it the full dictionary first time. On the last successful upload of data
 // run again with the first entry removed and repeate until empty
-var dictionaryQuery = function (dictionary, dailyset, client) {
+extractor.prototype.dictionaryQuery = function (dictionary, client) {
+  var Extractor = this
   setTimeout(function () {
     if (dictionary.entries.length) {
       var entry = dictionary.entries.shift()
-      dictionarySingleQuery(dailyset, entry, dictionary, client)
+      Extractor.dictionarySingleQuery(entry, dictionary, client)
     } else {
       console.log('finished extraction')
       finished()
@@ -45,10 +54,11 @@ var dictionaryQuery = function (dictionary, dailyset, client) {
   }, 0)
 }
 
-var dictionarySingleQuery = function (dailyset, entry, dictionary, client) {
+extractor.prototype.dictionarySingleQuery = function (entry, dictionary, client) {
+  var Extractor = this
   client.search({
-    index: 'fulltext',
-    type: 'unstructured',
+    index: Extractor.inputIndex,
+    type: Extractor.outputType,
     body: {
       _source: false,
       fields: ['cprojectID'],
@@ -70,19 +80,20 @@ var dictionarySingleQuery = function (dailyset, entry, dictionary, client) {
     }
     if (!error) {
       if (response.hits.hits.length === 0) {
-        dictionaryQuery(dictionary, 'foo', client)
+        Extractor.dictionaryQuery(dictionary, undefined, client)
       } else {
         for (var j = 0; j < response.hits.hits.length; j++) {
-          uploadOneDocFacts(response.hits.hits[j], dictionary, entry, client)
+          Extractor.uploadOneDocFacts(response.hits.hits[j], dictionary, entry, client)
         }
-        dictionaryQuery(dictionary, 'foo', client)
+        Extractor.dictionaryQuery(dictionary, undefined, client)
       }
     }
   })
 }
 
 // insert all the facts from one document as returned by ES
-var uploadOneDocFacts = function (oneDocFacts, dictionary, entry, client) {
+extractor.prototype.uploadOneDocFacts = function (oneDocFacts, dictionary, entry, client) {
+  var Extractor = this
   // console.log('snippet array is: ' + snippetArray)
   var snippetArray = oneDocFacts.highlight.fulltext
   for (var i = 0; i < snippetArray.length; i++) {
@@ -102,15 +113,16 @@ var uploadOneDocFacts = function (oneDocFacts, dictionary, entry, client) {
     fact.postfix = entities.decode(fact.postfix)
     fact.docId = oneDocFacts._id
     fact.cprojectID = oneDocFacts.fields.cprojectID
-    uploadOneFact(fact, dictionary, entry, client)
+    Extractor.uploadOneFact(fact, dictionary, entry, client)
   }
 }
 
-var uploadOneFact = function (fact, dictionary, entry, client) {
+extractor.prototype.uploadOneFact = function (fact, dictionary, entry, client) {
+  var Extractor = this
   // console.log("uploading one fact")
   client.create({
-    index: 'facts',
-    type: 'snippet',
+    index: Extractor.outputIndex,
+    type: Extractor.outputType,
     body: {
       'prefix': fact.prefix,
       'post': fact.postfix,
@@ -124,4 +136,4 @@ var uploadOneFact = function (fact, dictionary, entry, client) {
   })
 }
 
-module.exports.readDictionaries = readDictionaries
+module.exports = extractor

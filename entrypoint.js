23566638c0320d3ec303a4592386baaaa662ee6e
Thomas Arrow <thomasarrow@gmail.com>
2017-01-31 14:46:12 +0000

    use entrypoint.js as opposed to index.js fro module entry point

---
+++ entrypoint.js
@@ -0,0 +1,7 @@
+var index = require('./index.js')
+var retrieve = require('./retrieve.js')
+var extract = require('./extract.js')
+
+module.exports.index = index
+module.exports.retrieve = retrieve
+module.exports.extract = extract
